c251806d25f4d52fc1bea3d4bf2f422ceda5f2b4
Anne Flore Bernard <b.anneflore@yahoo.fr>
2024-04-10 12:03:16 +0200

    logic: routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import Header from "./components/common/Header.jsx";
+// import Button from "./components/common/Button.jsx";
+// import ButtonHeader from "./components/common/ButtonHeader.jsx";
+import AnimRoutes from "./components/AnimRoutes.jsx";
+// import Socials from "./components/Socials.jsx";
+import { BrowserRouter as Router } from "react-router-dom";
+// import { motion } from "framer-motion";
 
 function App() {
-  return <div className="text-red-500">yoyo</div>;
+  return (
+    <>
+      <Router>
+        <Header></Header>
+        <AnimRoutes></AnimRoutes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
--- src/components/AnimRoutes.jsx
+++ src/components/AnimRoutes.jsx
@@ -1,9 +1,33 @@
 import React from "react";
+import About from '../pages/About.jsx';
+import BodyPainting from '../pages/BodyPainting.jsx';
+import Ceramique from '../pages/Ceramique.jsx';
+import Contact from '../pages/Contact.jsx';
+import ExpoFestival from '../pages/ExpoFestival.jsx';
+import FlashDessins from '../pages/FlashDessins.jsx';
+import Home from '../pages/Home.jsx';
+import Serigraphie from '../pages/Serigraphie.jsx';
+import Tatoo from '../pages/Tatoo.jsx';
+import { Route, Routes, useLocation } from "react-router-dom";
+
+
+
+
 
 const AnimRoutes = () => {
-  return (
-   <div></div>
-  )
+  return <>
+  <Routes>
+    <Route path='/' element={<Home/>}></Route>
+    <Route path='/about' element={<About/>}></Route>
+    <Route path='/body' element={<BodyPainting/>}></Route>
+    <Route path='/expo' element={<ExpoFestival/>}></Route>
+    <Route path='/flash' element={<FlashDessins/>}></Route>
+    <Route path='/serigraphie' element={<Serigraphie/>}></Route>
+    <Route path='/contact' element={<Contact/>}></Route>
+    <Route path='/tatoo' element={<Tatoo/>}></Route>
+    <Route path='/ceramique' element={<Ceramique/>}></Route>
+  </Routes>  
+  </>
 }
 
 export default AnimRoutes;
