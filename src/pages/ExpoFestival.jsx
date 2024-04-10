aac182b0e2ef7d36c631b2ca6e448d47a465648c
Anne Flore Bernard <b.anneflore@yahoo.fr>
2024-04-10 19:12:31 +0200

    implementation du texte pages About et Expo

--- src/pages/ExpoFestival.jsx
+++ src/pages/ExpoFestival.jsx
@@ -2,7 +2,57 @@ import React from "react";
 
 const Expo = () => {
   return (
-    <h1>Expo / Festivals</h1>
+    <div className="bg-white w-full h-full pt-40 flex flex-col gap-y-8 lg:px-12  ">
+      <h1 className="font-semibold text-3xl flex items-center justify-center">Expo / Festivals </h1>
+    
+    <p><span className="text-lg font-semibold">2023 : </span>
+Performance body-painting, Festival Magdalena, Mas des Brousses, Montpellier
+Performance body-painting, Festival Ex Tenebris Lux, Opéra Comédie, Montpellier
+Performance body-painting, Festival Back to the trees, Fôret de Saint-Vit, Besançon
+Exposition collective, Galerie 411, Montpellier </p>
+<p>
+<span className="text-lg font-semibold">2022 : </span>
+Exposition et performance body-painting, « La maison des queers », La maison des
+choeurs, Montpellier
+Performance body-painting, Festival Ex Tenebris Lux, Opéra Comédie, Montpellier
+Performance body-painting, Festival Back to the trees, Fôret de Saint-Vit, Besançon
+Exposition collective, Galerie 411, Montpellier
+</p>
+<p>
+<span className="text-lg font-semibold">2021 : </span>Performance body-painting, Festival Ex Tenebris Lux, La maison des choeurs, Montpellier
+Performance body-painting, Festival Le printemps des ami.es, La Zone, Castries
+Exposition solo, « Nous allons revivre », Black Sheep, Montpellier
+</p>
+<p>
+<span className="text-lg font-semibold">2020 : </span>Exposition en duo, « Formes amoureuses », atelier Les émaill.é.es, Montpellier
+</p>
+<p>
+<span className="text-lg font-semibold">2019 : </span>Exposition collective «Appart’ expo», appartement privé, Montpellier
+Exposition collective «Les Biches», galerie Arterossa, Montpellier
+Exposition collective «Contrat social», Institut supérieur des Beaux-Arts, Besançon
+Exposition collective «La collab’», galerie «Arterossa», Montpellier
+Exposition collective «Archipel», galerie «52 rue Battant», Besançon
+Exposition collective «Sabbat», «Square cocoon», Montpellier
+Exposition en duo «Pam Pam Tatemae», espace Saint-Ravy, Montpellier
+</p>
+<p>
+<span className="text-lg font-semibold">2018 : </span>Exposition collective «Galerie éphémère #6», Villeneuve-lés-Maguelone
+Exposition personnelle «Tungsten74», atelier-galerie «Alabarak», Montpellier
+Exposition en duo «Equilibres contradictoires», galerie «Palais des paris», Japon
+(Takasaki)
+Exposition collective «Tous les printemps du monde», Fort Griffon, Besançon
+</p>
+<p>
+<span className="text-lg font-semibold">2017 : </span>Exposition collective & performance, festival d’art contemporain «Diep Heaven», Dieppe
+Performance en duo, festival «Back to the trees», Besançon
+</p>
+<p>
+<span className="text-lg font-semibold">2011-2016 : </span>Performances lors de trois éditions du festival «Excentricités» à l’Institut supérieur des
+Beaux-arts de Besançon, diverses sorties d’ateliers de recherche théâtrale et soirées
+performatives «Action building».
+</p>
+</div>
+
   )
 }
 
